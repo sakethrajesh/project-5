be2db6282797a0338a26316fb1121104de113222
Saketh Rajesh <69645417+sakethrajesh@users.noreply.github.com>
2021-11-17 14:47:59 -0800

    Add files via upload

---
+++ COVIDVisualizer/src/prj5/Input.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package prj5;
+
+/**
+ * @author Saketh
+ *
+ */
+public class Input {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
